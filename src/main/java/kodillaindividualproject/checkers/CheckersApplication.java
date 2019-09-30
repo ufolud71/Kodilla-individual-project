3422193dda28d0c16b5a2aa72833772458ec389f
ufolud71 <chudy.orfeusz@gmail.com>
2019-09-30 14:23:15 +0200

    checkers

---
+++ src/main/java/kodillaindividualproject/checkers/CheckersApplication.java
@@ -0,0 +1,13 @@
+package kodillaindividualproject.checkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckersApplication.class, args);
+    }
+
+}
